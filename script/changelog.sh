6e9a355df650434134bbfc447753525b88f3a0f5
Hector Rondon <hrondon@teclib.com>
2017-08-23 18:27:32 -0400

    fix(travis): catch TRAVIS_COMMIT_MESSAGE

--- script/changelog.sh
+++ script/changelog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo -----------------CHANGELOG-------------------
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo ---------------TRAVIS_BRANCH----------------
+    echo $TRAVIS_BRANCH
     echo ------------------CONFIG--------------------
     git config --global user.email $GH_EMAIL
     git config --global user.name "Travis CI"
@@ -9,11 +11,11 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     git remote add origin https://$GH_USER:$GH_TOKEN@github.com/d9iracd/travis.git
     echo ------------------RELEASE--------------------
     
-    if [[ $TRAVIS_COMMIT_MESSAGE != *"**version**"* ]]; then
+    if [[ $TRAVIS_COMMIT_MESSAGE != *"**version**"* && $TRAVIS_COMMIT_MESSAGE != *"**CHANGELOG.md**"* ]]; then
         git checkout master -f
     fi
     
-    npm run release -- -m "ci(release): generate #~CHANGELOG.md~# for version %s"
+    npm run release -- -m "ci(release): generate **CHANGELOG.md** for version %s"
     # # Get version number from package
     export GIT_TAG=$(jq -r ".version" package.json)
     conventional-github-releaser -t $GH_TOKEN -r 0
