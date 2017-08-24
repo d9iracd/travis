07a94b459dc98773e755ca5a055614fa0444adfa
Hector Rondon <hrondon@teclib.com>
2017-08-24 13:09:40 -0400

    feat(fix): create tag

--- script/changelog.sh
+++ script/changelog.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 echo ---------------TRAVIS_BRANCH----------------
     echo $TRAVIS_BRANCH
 echo -----------------CHANGELOG-------------------
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo ------------------CONFIG--------------------
+  echo ------------------CONFIG--------------------
     git config --global user.email $GH_EMAIL
     git config --global user.name "Travis CI"
     # git config --global push.followTags true
@@ -12,20 +12,21 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo ------------------RELEASE--------------------
     
     if [[ $TRAVIS_COMMIT_MESSAGE != *"**version**"* && $TRAVIS_COMMIT_MESSAGE != *"**CHANGELOG.md**"* ]]; then
-        git checkout master -f
+        git checkout $TRAVIS_BRANCH -f
 
         npm run release -- -m "ci(release): generate **CHANGELOG.md** for version %s"
         # # Get version number from package
         export GIT_TAG=$(jq -r ".version" package.json)
-        conventional-github-releaser -t $GH_TOKEN -r 0
+        git push origin ${GIT_TAG}
+        # conventional-github-releaser -t $GH_TOKEN
         # # Update CFBundleShortVersionString   
         echo $GIT_TAG >> tag.txt
-        git add -A
+        git add tag.txt
         # Rename last commit
         git commit -m "ci(build): increment **version** ${GIT_TAG}"
 
-        git push origin master
+        git push origin $TRAVIS_BRANCH
+        
+        # fastlane release
     fi
-else
-    echo $TRAVIS_BRANCH
 fi
